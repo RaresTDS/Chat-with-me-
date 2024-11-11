38e9fbe32a193085236c8c51c5fd6f7963fdf62f
stefania.tamasoi <stefaniatamasoi9@gmail.com.>
2024-11-11 16:58:27 +0200

    var1stefi

---
+++ Chat-with-me--master/src/main/java/org/example/multimediachat/model/Message.java
@@ -0,0 +1,4 @@
+package org.example.multimediachat.model;
+
+public class Message {
+}
---
+++ Chat-with-me--master/src/test/java/org/example/multimediachat/MultimediaChatApplicationTests.java
@@ -0,0 +1,11 @@
+package org.example.multimediachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimediaChatApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
